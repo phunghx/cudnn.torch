ed8b199623b50f6c72a02f51e58a6cc682e23994
phunghx <phunghx@gmail.com>
2016-12-09 17:27:17 +0900

    first commit

---
+++ phnnTest.lua
@@ -0,0 +1,6 @@
+local phnnTest, parent = torch.class('cudnn.ReLU','cudnn._Pointwise')
+
+function phnnTest:updateOutput(input)
+  if not self.mode then self.mode = 'CUDNN_ACTIVATION_RELU' end
+  return parent.updateOutput(self, input)
+end
